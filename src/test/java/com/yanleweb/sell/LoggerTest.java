973fbf6cde40552842f66b38a6fe00c0dad45775
yanlele <331393627@qq.com>
2019-07-26 07:52:05 +0800

    Initial commit

---
+++ src/test/java/com/yanleweb/sell/LoggerTest.java
@@ -0,0 +1,13 @@
+package com.yanleweb.sell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class LoggerTest {
+    @Test
+    public void test1() {
+        
+    }
+}

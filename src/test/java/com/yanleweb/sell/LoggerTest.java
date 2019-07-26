5e816fb1d02101466de2f5f6e72e3d4c13a04cd6
yanlele <331393627@qq.com>
2019-07-26 08:06:40 +0800

    [+]lombok

--- src/test/java/com/yanleweb/sell/LoggerTest.java
+++ src/test/java/com/yanleweb/sell/LoggerTest.java
@@ -1,5 +1,6 @@
 package com.yanleweb.sell;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,13 +10,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class LoggerTest {
-    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+//    这种方式稍显麻烦 有更加简单的办法 ， 见lombok, 当然还需要下载一个idea 的插件
+//    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1() {
-        logger.debug("debug...");
-        logger.info("info...");
-        logger.error("error");
+//        logger.debug("debug...");
+//        logger.info("info...");
+//        logger.error("error");
+
+        String name ="yanle";
+        String password = "yanle123";
+        log.debug("debug...");
+        log.info("name: {}, password: {}", name, password);
+        log.info("info...");
+        log.error("error");
     }
 }

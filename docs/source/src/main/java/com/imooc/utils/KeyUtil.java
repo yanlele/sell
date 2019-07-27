567328086e80c3216ad9366aa00bc904f3dd6c98
yanlele <331393627@qq.com>
2019-07-27 22:42:48 +0800

    [*]

--- docs/source/src/main/java/com/imooc/utils/KeyUtil.java
+++ docs/source/src/main/java/com/imooc/utils/KeyUtil.java
@@ -15,6 +15,8 @@ public class KeyUtil {
      */
     public static synchronized String genUniqueKey() {
         Random random = new Random();
+
+        // 六位随机数
         Integer number = random.nextInt(900000) + 100000;
 
         return System.currentTimeMillis() + String.valueOf(number);

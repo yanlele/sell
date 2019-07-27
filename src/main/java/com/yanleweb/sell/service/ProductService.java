bc5b4e20f265909b3fd5e638abe2c4f8d86eed1f
yanlele <331393627@qq.com>
2019-07-27 17:16:56 +0800

    [+]ProductService

--- src/main/java/com/yanleweb/sell/service/ProductService.java
+++ src/main/java/com/yanleweb/sell/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.yanleweb.sell.service;
 
+import com.yanleweb.sell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface ProductService {
+
+    ProductInfo findOne(String productId);
+
+    // 上架的物品
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
 }

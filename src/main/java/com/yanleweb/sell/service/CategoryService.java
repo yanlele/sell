6db1c552f12e776a7533bdab47d497ff98bc1905
yanlele <331393627@qq.com>
2019-07-27 12:11:52 +0800

    [+]finish service

--- src/main/java/com/yanleweb/sell/service/CategoryService.java
+++ src/main/java/com/yanleweb/sell/service/CategoryService.java
@@ -1,4 +1,15 @@
 package com.yanleweb.sell.service;
 
+import com.yanleweb.sell.dataobject.ProductCategory;
+
+import java.util.List;
+
 public interface CategoryService {
+    ProductCategory findOne(Integer categoryId);
+
+    List<ProductCategory> findAll();
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+
+    ProductCategory save(ProductCategory productCategory);
 }
--- src/main/java/com/yanleweb/sell/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/yanleweb/sell/service/impl/CategoryServiceImpl.java
@@ -1,4 +1,35 @@
 package com.yanleweb.sell.service.impl;
 
-public class CategoryServiceImpl {
+import com.yanleweb.sell.dataobject.ProductCategory;
+import com.yanleweb.sell.repository.ProductCategoryRepository;
+import com.yanleweb.sell.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private ProductCategoryRepository repository;
+
+    @Override
+    public ProductCategory findOne(Integer categoryId) {
+        return repository.findById(categoryId).get();
+    }
+
+    @Override
+    public List<ProductCategory> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList) {
+        return repository.findByCategoryTypeIn(categoryTypeList);
+    }
+
+    @Override
+    public ProductCategory save(ProductCategory productCategory) {
+        return repository.save(productCategory);
+    }
 }

e2413e241919b7ccc9ceed978181239acd30d141
yanlele <331393627@qq.com>
2019-07-27 17:23:46 +0800

    [+]ProductServiceImplTest

---
+++ src/test/java/com/yanleweb/sell/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.yanleweb.sell.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductServiceImplTest {
+
+    @Test
+    public void findOne() {
+    }
+
+    @Test
+    public void findUpAll() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void save() {
+    }
+}

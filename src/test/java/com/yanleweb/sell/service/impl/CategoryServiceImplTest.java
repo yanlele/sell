6a801e9c9a49a643db6bd4f5ab5d0692fd65a5a7
yanlele <331393627@qq.com>
2019-07-27 13:12:51 +0800

    [+]test

---
+++ src/test/java/com/yanleweb/sell/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.yanleweb.sell.service.impl;
+
+import com.yanleweb.sell.dataobject.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryServiceImplTest {
+    @Autowired
+    private CategoryServiceImpl categoryService;
+
+    @Test
+    public void findOne() throws Exception {
+        ProductCategory productCategory = categoryService.findOne(1);
+        Assert.assertEquals(new Integer(1), productCategory.getCategoryId());
+    }
+
+    @Test
+    public void findAll() {
+        List<ProductCategory> productCategoryList = categoryService.findAll();
+        Assert.assertNotEquals(0, productCategoryList.size());
+    }
+
+    @Test
+    public void findByCategoryTypeIn() {
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1, 2, 3));
+        Assert.assertNotEquals(0, productCategoryList.size());
+    }
+
+    @Test
+    public void save() {
+        ProductCategory productCategory = new ProductCategory("lellele", 2);
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void update() {
+        ProductCategory productCategory = categoryService.findOne(4);
+        productCategory.setCategoryName("boyboyboy11111");
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertEquals(productCategory, result);
+    }
+}

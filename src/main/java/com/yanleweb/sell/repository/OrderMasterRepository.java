5d740e36ef5bb846808a42f6d6922bc99d597843
yanlele <331393627@qq.com>
2019-07-29 09:15:05 +0800

    impl test

--- src/main/java/com/yanleweb/sell/repository/OrderMasterRepository.java
+++ src/main/java/com/yanleweb/sell/repository/OrderMasterRepository.java
@@ -1,4 +1,11 @@
 package com.yanleweb.sell.repository;
 
-public interface OrderMasterRepository {
+import com.yanleweb.sell.dataobject.OrderMaster;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface OrderMasterRepository extends JpaRepository<OrderMaster, String> {
+    Page<OrderMaster> findByBuyerOpenid(String buyerOpenid, Pageable pageable);
 }
--- src/test/java/com/yanleweb/sell/repository/OrderMasterRepositoryTest.java
+++ src/test/java/com/yanleweb/sell/repository/OrderMasterRepositoryTest.java
@@ -1,5 +1,47 @@
-import static org.junit.Assert.*;
+package com.yanleweb.sell.repository;
 
+import com.yanleweb.sell.dataobject.OrderMaster;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
 public class OrderMasterRepositoryTest {
 
+    @Autowired
+    private OrderMasterRepository repository;
+
+    private final String OPENID = "1234567890";
+
+    @Test
+    public void save() {
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234568");
+        orderMaster.setBuyerName("yanle");
+        orderMaster.setBuyerPhone("123659283234");
+        orderMaster.setBuyerAddress("成都");
+        orderMaster.setBuyerOpenid(OPENID);
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
+
+        OrderMaster result = repository.save(orderMaster);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByBuyerOpenid() {
+        PageRequest pageRequest = PageRequest.of(1, 3);
+        Page<OrderMaster> orderMasterPage = repository.findByBuyerOpenid(OPENID, pageRequest);
+        System.out.println(orderMasterPage);
+        Assert.assertNotEquals(0, orderMasterPage.getTotalElements());
+    }
 }

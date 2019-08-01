5f0a5d119db72b26979667f2b57d2fd1a71bdef2
yanlele <331393627@qq.com>
2019-08-01 09:22:39 +0800

    [+]OrderDetailRepositoryTest

---
+++ src/test/java/com/yanleweb/sell/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.yanleweb.sell.repository;
+
+import com.yanleweb.sell.dataobject.OrderDetail;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrderDetailRepositoryTest {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void saveTest() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setDetailId("1234567810");
+        orderDetail.setOrderId("11111112");
+        orderDetail.setProductIcon("http://xxxx.jpg");
+        orderDetail.setProductId("11111112");
+        orderDetail.setProductName("皮蛋粥");
+        orderDetail.setProductPrice(new BigDecimal(2.2));
+        orderDetail.setProductQuantity(3);
+
+        OrderDetail result = orderDetailRepository.save(orderDetail);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByOrderId() throws Exception {
+        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderId("11111112");
+        Assert.assertNotEquals(0, orderDetailList.size());
+    }
+}

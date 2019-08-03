aa240e0f5fc05bf052c1d60974866115ecd5ed76
yanlele <331393627@qq.com>
2019-08-03 22:20:04 +0800

    [+]添加mybatis

---
+++ docs/source/src/main/java/com/imooc/dataobject/mapper/ProductCategoryMapper.java
@@ -0,0 +1,11 @@
+package com.imooc.dataobject.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+
+import java.util.Map;
+
+public interface ProductCategoryMapper {
+
+    @Insert("insert into product_category(category_name, category_type) values(#{category_name, jdbcType=VARCHAR}, #{category_type, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+}

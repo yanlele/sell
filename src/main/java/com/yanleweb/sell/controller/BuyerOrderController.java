36e2cc711036650b1bd553e5e1ffde23cdbcf0bd
yanlele <331393627@qq.com>
2019-08-10 22:43:11 +0800

    [+]create

--- src/main/java/com/yanleweb/sell/controller/BuyerOrderController.java
+++ src/main/java/com/yanleweb/sell/controller/BuyerOrderController.java
@@ -2,19 +2,24 @@ package com.yanleweb.sell.controller;
 
 
 import com.yanleweb.sell.VO.ResultVO;
+import com.yanleweb.sell.converter.OrderForm2OrderDTOConverter;
+import com.yanleweb.sell.dto.OrderDTO;
 import com.yanleweb.sell.enums.ResultEnum;
 import com.yanleweb.sell.exception.SellException;
 import com.yanleweb.sell.form.OrderForm;
 import com.yanleweb.sell.service.BuyerService;
 import com.yanleweb.sell.service.OrderService;
+import com.yanleweb.sell.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -39,6 +44,17 @@ public class BuyerOrderController {
             );
         }
 
-        return null;
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
+            log.error("【创建订单】购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+
+        OrderDTO createResult = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", createResult.getOrderId());
+
+        return ResultVOUtil.success(map);
     }
 }

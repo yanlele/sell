e2a0fd38bdc9cbbaded5ccde63abfaedf5bd76fa
yanlele <331393627@qq.com>
2019-08-10 22:33:46 +0800

    [+]add dependency

---
+++ src/main/java/com/yanleweb/sell/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,13 @@
+package com.yanleweb.sell.converter;
+
+import com.yanleweb.sell.dto.OrderDTO;
+import com.yanleweb.sell.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+    public static OrderDTO convert(OrderForm orderForm) {
+        return null;
+    }
+
+}

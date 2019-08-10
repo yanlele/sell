2c8a2942901a129fa105a99f16564807ea681a97
yanlele <331393627@qq.com>
2019-08-10 23:24:48 +0800

    [+]ResultVO

--- src/main/java/com/yanleweb/sell/controller/BuyerOrderController.java
+++ src/main/java/com/yanleweb/sell/controller/BuyerOrderController.java
@@ -12,14 +12,16 @@ import com.yanleweb.sell.service.OrderService;
 import com.yanleweb.sell.utils.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -57,4 +59,23 @@ public class BuyerOrderController {
 
         return ResultVOUtil.success(map);
     }
+
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(
+            @RequestParam("openid") String openid,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        if (StringUtils.isEmpty(openid)) {
+            log.error("【查询订单列表】openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+
+        PageRequest request = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
+
+
 }

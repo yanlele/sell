6999e1a89573ef59371cc924a82bfad6068f6932
yanlele <331393627@qq.com>
2019-08-10 22:38:33 +0800

    [+]OrderForm2OrderDTOConverter

--- src/main/java/com/yanleweb/sell/converter/OrderForm2OrderDTOConverter.java
+++ src/main/java/com/yanleweb/sell/converter/OrderForm2OrderDTOConverter.java
@@ -1,13 +1,43 @@
 package com.yanleweb.sell.converter;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.yanleweb.sell.dataobject.OrderDetail;
 import com.yanleweb.sell.dto.OrderDTO;
+import com.yanleweb.sell.enums.ResultEnum;
+import com.yanleweb.sell.exception.SellException;
 import com.yanleweb.sell.form.OrderForm;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class OrderForm2OrderDTOConverter {
     public static OrderDTO convert(OrderForm orderForm) {
-        return null;
+        Gson gson = new Gson();
+        OrderDTO orderDTO = new OrderDTO();
+
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+
+        try {
+            orderDetailList = gson.fromJson(
+                    orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>() {
+                    }.getType()
+            );
+        } catch (Exception e) {
+            log.error("【对象转换】错误, string={}", orderForm.getItems());
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
     }
 
 }

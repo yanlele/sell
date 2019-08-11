4221b1fe71603b36c465c3f2c5833ec07c7b373e
yanlele <331393627@qq.com>
2019-08-11 23:26:29 +0800

    [+]add config

--- src/main/java/com/yanleweb/sell/service/impl/PayServiceImpl.java
+++ src/main/java/com/yanleweb/sell/service/impl/PayServiceImpl.java
@@ -1,10 +1,27 @@
 package com.yanleweb.sell.service.impl;
 
 import com.lly835.bestpay.model.PayResponse;
+import com.lly835.bestpay.service.impl.BestPayServiceImpl;
 import com.yanleweb.sell.dto.OrderDTO;
+import com.yanleweb.sell.service.OrderService;
 import com.yanleweb.sell.service.PayService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@Slf4j
 public class PayServiceImpl implements PayService {
+
+    private static final String ORDER_NAME = "微信点餐订单";
+
+    @Autowired
+    private BestPayServiceImpl bestPayService;
+
+    @Autowired
+    private OrderService orderService;
+
     @Override
     public PayResponse crate(OrderDTO orderDTO) {
         return null;

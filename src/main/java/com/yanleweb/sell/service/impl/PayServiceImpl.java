c94220037236b5c3dfa687b6612d9498923971e6
yanlele <331393627@qq.com>
2019-08-11 23:30:17 +0800

    [+]JsonUtil

--- src/main/java/com/yanleweb/sell/service/impl/PayServiceImpl.java
+++ src/main/java/com/yanleweb/sell/service/impl/PayServiceImpl.java
@@ -1,7 +1,10 @@
 package com.yanleweb.sell.service.impl;
 
+import com.lly835.bestpay.enums.BestPayTypeEnum;
+import com.lly835.bestpay.model.PayRequest;
 import com.lly835.bestpay.model.PayResponse;
 import com.lly835.bestpay.service.impl.BestPayServiceImpl;
+import com.lly835.bestpay.utils.JsonUtil;
 import com.yanleweb.sell.dto.OrderDTO;
 import com.yanleweb.sell.service.OrderService;
 import com.yanleweb.sell.service.PayService;
@@ -24,6 +27,14 @@ public class PayServiceImpl implements PayService {
 
     @Override
     public PayResponse crate(OrderDTO orderDTO) {
+        PayRequest payRequest = new PayRequest();
+        payRequest.setOpenid(orderDTO.getBuyerOpenid());
+        payRequest.setOrderAmount(orderDTO.getOrderAmount().doubleValue());
+        payRequest.setOrderId(orderDTO.getOrderId());
+        payRequest.setOrderName(ORDER_NAME);
+        payRequest.setPayTypeEnum(BestPayTypeEnum.WXPAY_H5);
+        log.info("【微信支付】发起支付, request={}", JsonUtil.toJson(payRequest));
+
         return null;
     }
 

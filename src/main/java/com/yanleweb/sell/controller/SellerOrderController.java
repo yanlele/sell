1e64a046f8bbb2958c6df477ccbc71ffca8ca5c0
yanlele <331393627@qq.com>
2019-08-11 23:54:21 +0800

    [+]SellerOrderController init

---
+++ src/main/java/com/yanleweb/sell/controller/SellerOrderController.java
@@ -0,0 +1,40 @@
+package com.yanleweb.sell.controller;
+
+
+import com.yanleweb.sell.dto.OrderDTO;
+import com.yanleweb.sell.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/order")
+@Slf4j
+public class SellerOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/list")
+    public ModelAndView list(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            Map<String, Object> map
+    ) {
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(pageRequest);
+        map.put("orderDTOPage", orderDTOPage);
+        map.put("currentPage", page);
+        map.put("size", size);
+
+        return new ModelAndView("order/list", map);
+    }
+}

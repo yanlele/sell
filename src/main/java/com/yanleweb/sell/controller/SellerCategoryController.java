ecb071d9fa90bbb9fc41c898c2554da4ad4b2d7c
yanlele <331393627@qq.com>
2019-08-14 22:21:12 +0800

    [+]商品index

--- src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
+++ src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,9 +21,22 @@ public class SellerCategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/list")
-    public ModelAndView list(Map<String , Object> map) {
+    public ModelAndView list(Map<String, Object> map) {
         List<ProductCategory> categoryList = categoryService.findAll();
         map.put("categoryList", categoryList);
         return new ModelAndView("category/list", map);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(
+            @RequestParam(value = "categoryId", required = false) Integer categoryId,
+            Map<String, Object> map
+    ) {
+        if (categoryId != null) {
+            ProductCategory productCategory = categoryService.findOne(categoryId);
+            map.put("category", productCategory);
+        }
+
+        return new ModelAndView("category/index", map);
     }
 }

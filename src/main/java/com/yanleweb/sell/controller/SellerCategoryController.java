24d59c8e7d454bb8d2cf30532899c0e03b6f006d
yanlele <331393627@qq.com>
2019-08-14 22:18:07 +0800

    [+]list

--- src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
+++ src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
@@ -1,10 +1,16 @@
 package com.yanleweb.sell.controller;
 
 
+import com.yanleweb.sell.dataobject.ProductCategory;
 import com.yanleweb.sell.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/seller/category")
@@ -12,4 +18,11 @@ public class SellerCategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/list")
+    public ModelAndView list(Map<String , Object> map) {
+        List<ProductCategory> categoryList = categoryService.findAll();
+        map.put("categoryList", categoryList);
+        return new ModelAndView("category/list", map);
+    }
 }

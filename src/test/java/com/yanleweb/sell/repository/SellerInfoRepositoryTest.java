911bc0037aa7e21636622f6ef2fdcfdf9ac83ba8
yanlele <331393627@qq.com>
2019-08-14 22:36:57 +0800

    [+]SellerInfoRepositoryTest

---
+++ src/test/java/com/yanleweb/sell/repository/SellerInfoRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.yanleweb.sell.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SellerInfoRepositoryTest {
+
+
+
+    @Test
+    public void findByOpenid() {
+    }
+}

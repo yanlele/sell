215f57864e059c574d9f73d1f264cedde6415a77
yanlele <331393627@qq.com>
2019-08-14 22:31:11 +0800

    [+]保存种类

--- src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
+++ src/main/java/com/yanleweb/sell/controller/SellerCategoryController.java
@@ -2,14 +2,19 @@ package com.yanleweb.sell.controller;
 
 
 import com.yanleweb.sell.dataobject.ProductCategory;
+import com.yanleweb.sell.form.CategoryForm;
 import com.yanleweb.sell.service.CategoryService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -38,5 +43,33 @@ public class SellerCategoryController {
         }
 
         return new ModelAndView("category/index", map);
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(
+            @Valid CategoryForm form,
+            BindingResult bindingResult,
+            Map<String, Object> map
+    ) {
+        if (bindingResult.hasErrors()) {
+            map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+            map.put("urm", "/sell/seller/category/index");
+            return new ModelAndView("common/error", map);
+        }
+        ProductCategory productCategory = new ProductCategory();
+        try {
+            if (form.getCategoryId() != null) {
+                productCategory = categoryService.findOne(form.getCategoryId());
+            }
+            BeanUtils.copyProperties(form, productCategory);
+            categoryService.save(productCategory);
+        } catch (Exception e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/category/index");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/category/list");
+        return new ModelAndView("common/success", map);
     }
 }

d2bdf574d97b7f1d61959dbfa76c43295be0536d
yanlele <331393627@qq.com>
2019-08-24 00:31:12 +0800

    [+]添加卖家信息

--- src/test/java/com/yanleweb/sell/repository/SellerInfoRepositoryTest.java
+++ src/test/java/com/yanleweb/sell/repository/SellerInfoRepositoryTest.java
@@ -1,12 +1,27 @@
 package com.yanleweb.sell.repository;
 
+import com.yanleweb.sell.dataobject.SellerInfo;
+import com.yanleweb.sell.utils.KeyUtil;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SellerInfoRepositoryTest {
 
+    @Autowired
+    private SellerInfoRepository repository;
 
+    @Test
+    public void save() {
+        SellerInfo sellerInfo = new SellerInfo();
+        sellerInfo.setSellerId(KeyUtil.genUniqueKey());
+        sellerInfo.setUsername("admin");
+        sellerInfo.setPassword("admin");
+        sellerInfo.setOpenid("abcd");
+
+        SellerInfo result = repository.save(sellerInfo);
+        Assert.assertNotNull(result);
+    }
 
     @Test
     public void findByOpenid() {

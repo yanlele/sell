377c5651508e60ab2ce139b998df5ae1c8c0747f
yanlele <331393627@qq.com>
2019-08-24 00:41:36 +0800

    [+]SellerServiceImplTest

---
+++ src/test/java/com/yanleweb/sell/service/impl/SellerServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.yanleweb.sell.service.impl;
+
+import com.yanleweb.sell.dataobject.SellerInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SellerServiceImplTest {
+
+    private static final String openid = "abcd";
+
+    @Autowired
+    private SellerServiceImpl sellerService;
+
+    @Test
+    public void findSellerInfoByOpenid() {
+        SellerInfo result = sellerService.findSellerInfoByOpenid(openid);
+        Assert.assertEquals(openid, result.getOpenid());
+    }
+}

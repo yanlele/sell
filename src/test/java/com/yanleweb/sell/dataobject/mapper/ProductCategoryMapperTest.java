7ffbf40b68fa01bd525cff8afc9d0f2bbe618c93
yanlele <331393627@qq.com>
2019-08-24 23:07:49 +0800

    [+]测试

---
+++ src/test/java/com/yanleweb/sell/dataobject/mapper/ProductCategoryMapperTest.java
@@ -0,0 +1,40 @@
+package com.yanleweb.sell.dataobject.mapper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductCategoryMapperTest {
+
+    @Test
+    public void insertByMap() {
+    }
+
+    @Test
+    public void insertByObject() {
+    }
+
+    @Test
+    public void findByCategoryType() {
+    }
+
+    @Test
+    public void findByCategoryName() {
+    }
+
+    @Test
+    public void updateByCategoryType() {
+    }
+
+    @Test
+    public void updateByCategoryObject() {
+    }
+
+    @Test
+    public void deleteProductCategory() {
+    }
+
+    @Test
+    public void selectByCategoryType() {
+    }
+}

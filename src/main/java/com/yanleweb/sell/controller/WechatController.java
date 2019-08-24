5f8cf52b36a18ef658a8d808fa37437a296b13be
yanlele <331393627@qq.com>
2019-08-24 12:47:32 +0800

    [+]authorize

--- src/main/java/com/yanleweb/sell/controller/WechatController.java
+++ src/main/java/com/yanleweb/sell/controller/WechatController.java
@@ -1,10 +1,16 @@
 package com.yanleweb.sell.controller;
 
+import com.yanleweb.sell.config.ProjectUrlConfig;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.mp.api.WxMpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.URLEncoder;
 
 @Controller
 @RequestMapping("/wechat")
@@ -17,5 +23,14 @@ public class WechatController {
     @Autowired
     private WxMpService wxOpenService;
 
+    @Autowired
+    private ProjectUrlConfig projectUrlConfig;
+
+    @GetMapping("/authorize")
+    public String authorize(@RequestParam("returnUrl") String returnUrl) {
+        String url = projectUrlConfig.getWechatMpAuthorize() + "/sell/wechat/userInfo";
+        String redirectUlr = wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAUTH2_SCOPE_BASE, URLEncoder.encode(returnUrl));
+        return "redirect:" + redirectUlr;
+    }
 
 }

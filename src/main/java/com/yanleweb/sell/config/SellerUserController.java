9f092c4805e451f0c8791bdd32ec4aa39a26d245
yanlele <331393627@qq.com>
2019-08-24 13:40:19 +0800

    [+]退出登录

--- src/main/java/com/yanleweb/sell/config/SellerUserController.java
+++ src/main/java/com/yanleweb/sell/config/SellerUserController.java
@@ -1,10 +1,12 @@
 package com.yanleweb.sell.config;
 
 
+import com.yanleweb.sell.constant.CookieConstant;
 import com.yanleweb.sell.constant.RedisConstant;
 import com.yanleweb.sell.dataobject.SellerInfo;
 import com.yanleweb.sell.enums.ResultEnum;
 import com.yanleweb.sell.service.SellerService;
+import com.yanleweb.sell.utils.CookieUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -33,7 +37,7 @@ public class SellerUserController {
     @GetMapping("/login")
     public ModelAndView login(
             @RequestParam("openid") String openid,
-            HttpServletRequest response,
+            HttpServletResponse response,
             Map<String, Object> map
     ) {
         // 1. openid 去和数据库的数据匹配
@@ -46,10 +50,31 @@ public class SellerUserController {
 
         // 设置token 到redis
         String token = UUID.randomUUID().toString();
-        Integer expire  = RedisConstant.EXPIRE;
+        Integer expire = RedisConstant.EXPIRE;
 
         redisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_PREFIX, token), openid, expire, TimeUnit.SECONDS);
 
         // 设置token 到 cookie
+        CookieUtil.set(response, CookieConstant.TOKEN, token, expire);
+        return new ModelAndView("redirect:" + projectUrlConfig.getSell() + "/sell/seller/order/list");
+    }
+
+    @GetMapping("/logout")
+    public ModelAndView logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Map<String, Object> map
+    ) {
+        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+        if (cookie != null) {
+            redisTemplate.opsForValue().getOperations().delete(String.format(RedisConstant.TOKEN_PREFIX, cookie.getValue()));
+
+            CookieUtil.set(response, CookieConstant.TOKEN, null, 0);
+        }
+
+        map.put("msg", ResultEnum.LOGOUT_SUCCESS.getMessage());
+        map.put("url", "/sell/seller/order/list");
+
+        return new ModelAndView("common/success", map);
     }
 }

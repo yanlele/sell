3abbc21b58890e39d9860ffd722a4c3a40860418
yanlele <331393627@qq.com>
2019-08-24 13:23:08 +0800

    [+]SellerUserController

---
+++ src/main/java/com/yanleweb/sell/config/SellerUserController.java
@@ -0,0 +1,55 @@
+package com.yanleweb.sell.config;
+
+
+import com.yanleweb.sell.constant.RedisConstant;
+import com.yanleweb.sell.dataobject.SellerInfo;
+import com.yanleweb.sell.enums.ResultEnum;
+import com.yanleweb.sell.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Controller
+@RequestMapping("/seller")
+public class SellerUserController {
+    @Autowired
+    private SellerService sellerService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private ProjectUrlConfig projectUrlConfig;
+
+    @GetMapping("/login")
+    public ModelAndView login(
+            @RequestParam("openid") String openid,
+            HttpServletRequest response,
+            Map<String, Object> map
+    ) {
+        // 1. openid 去和数据库的数据匹配
+        SellerInfo sellerInfo = sellerService.findSellerInfoByOpenid(openid);
+        if (sellerInfo == null) {
+            map.put("msg", ResultEnum.LOGIN_FAIL.getMessage());
+            map.put("url", "/sell/seller/order/list");
+            return new ModelAndView("common/error");
+        }
+
+        // 设置token 到redis
+        String token = UUID.randomUUID().toString();
+        Integer expire  = RedisConstant.EXPIRE;
+
+        redisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_PREFIX, token), openid, expire, TimeUnit.SECONDS);
+
+        // 谁知token 到 cookie
+    }
+}
---
+++ src/main/java/com/yanleweb/sell/constant/RedisConstant.java
@@ -0,0 +1,8 @@
+package com.yanleweb.sell.constant;
+
+public interface RedisConstant {
+
+    public final String TOKEN_PREFIX = "token_%s";
+
+    public final Integer EXPIRE = 7200; // 2 小哥
+}

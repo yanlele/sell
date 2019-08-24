ad707612128b4d2d58b5670e1e3ba1ad3a7b4535
yanlele <331393627@qq.com>
2019-08-24 12:53:03 +0800

    [+]userInfo

--- src/main/java/com/yanleweb/sell/controller/WechatController.java
+++ src/main/java/com/yanleweb/sell/controller/WechatController.java
@@ -1,9 +1,13 @@
 package com.yanleweb.sell.controller;
 
 import com.yanleweb.sell.config.ProjectUrlConfig;
+import com.yanleweb.sell.enums.ResultEnum;
+import com.yanleweb.sell.exception.SellException;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,5 +36,23 @@ public class WechatController {
         String redirectUlr = wxMpService.oauth2buildAuthorizationUrl(url, WxConsts.OAUTH2_SCOPE_BASE, URLEncoder.encode(returnUrl));
         return "redirect:" + redirectUlr;
     }
+
+    @GetMapping("/userInfo")
+    public String userInfo(
+            @RequestParam("code") String code,
+            @RequestParam("state") String returnUrl
+    ) {
+        WxMpOAuth2AccessToken wxMpOAuth2AccessToken;
+        try {
+            wxMpOAuth2AccessToken = wxMpService.oauth2getAccessToken(code);
+        } catch (WxErrorException e) {
+            log.error("[微信授权失败] {}", e);
+            throw new SellException(ResultEnum.WECHAT_MP_ERROR.getCode(), e.getError().getErrorMsg());
+        }
+
+        String openId = wxMpOAuth2AccessToken.getOpenId();
+        return "redirect:" + returnUrl + "?openid=" + openId;
+    }
+
 
 }
